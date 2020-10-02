87cb996275f6e496713f4a36dcb02e3eaa043692
haihoang96 <hoanghai129612@gmail.com>
2020-10-02 15:17:04 +0700

    Hotfix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import Hero from './Hero/Hero';
+import Info from './Info/Info';
 
 const Home = () => {
   return (
     <Fragment>
       <Hero />
+      <Info />
     </Fragment>
   );
 };
